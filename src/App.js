e66397c9b473eb0a4dae5d65536fcc6ed29335e0
Achraf Kaou <achraf03achref@gmail.com>
2024-04-29 07:09:07 +0100

    suppression des components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navigation from './components/navbar';
 import Shop from './components/shop';
 import Footer from './components/footer';
 import ShoppingCart from './components/shoppingCart';
-import Confirm from './components/confirm';
 import Admin from './components/admin';
 import Addprod from './components/addprod';
 import Cart from './components/navbarCart';
@@ -37,7 +36,6 @@ function App() {
         <Route path="/admin" element={<Admin />}  />
         <Route path="/addProd" element={<Addprod />} />
         <Route path="/navbarCart" element={<Cart />} />
-        <Route path="/confirm" element={<Confirm />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path='/minicart' element={<MiniCart/>} />
